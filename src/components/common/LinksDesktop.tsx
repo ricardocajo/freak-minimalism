481e9f19f46d2c4bf554989f994cd31fc31c2a9a
Ricardo Cordeiro <82663158+ricardocajo@users.noreply.github.com>
2025-06-19 09:33:55 +0100

    cart solved

--- src/components/common/LinksDesktop.tsx
+++ src/components/common/LinksDesktop.tsx
@@ -59,13 +59,13 @@ export function LinksDesktop() {
 }
 
 const ListItem = React.forwardRef<
-  React.ElementRef<"a">,
-  React.ComponentPropsWithoutRef<"a">
+  React.ElementRef<typeof Link>,
+  React.ComponentPropsWithoutRef<typeof Link>
 >(({ className, title, children, ...props }, ref) => {
   return (
     <li>
       <NavigationMenuLink asChild>
-        <a
+        <Link
           ref={ref}
           className={cn(
             "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground hover:bg-[#1F1F1F]",
@@ -79,7 +79,7 @@ const ListItem = React.forwardRef<
           <p className="text-sm leading-snug line-clamp-2 text-muted-foreground text-[#A1A1A1]">
             {children}
           </p>
-        </a>
+        </Link>
       </NavigationMenuLink>
     </li>
   );
